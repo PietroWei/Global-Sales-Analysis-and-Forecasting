ffe8eeb180f5fad4fbd2e9103c06b640230d039b
PietroWei <97468292+PietroWei@users.noreply.github.com>
2025-03-12 13:28:22 +0000

    delete some folder

--- src/model_building.py
+++ src/model_building.py
@@ -2,6 +2,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import joblib
+import pandas as pd
+import numpy as np
+import os
 
 def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
